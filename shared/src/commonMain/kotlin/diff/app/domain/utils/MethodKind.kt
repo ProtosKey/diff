ad8b3f9526e82b935d3a2cdeb9a14efabb6ce72a
Rinat <rinat.gazizullin.31@bk.ru>
2026-06-17 14:44:11 +0300

    feat: added short name for methods

--- shared/src/commonMain/kotlin/diff/app/domain/utils/MethodKind.kt
+++ shared/src/commonMain/kotlin/diff/app/domain/utils/MethodKind.kt
@@ -1,7 +1,11 @@
 package diff.app.domain.utils
 
-enum class MethodKind(val label: String, val order: Int) {
-    EULER(label = "Усовершенствованный метод Эйлера", order = 2),
-    RUNGE(label = "Метод Рунге-Кутта 4-го порядка", order = 4),
-    MILNE(label = "Метод Милна", order = 4),
+enum class MethodKind(
+    val label: String,
+    val short: String,
+    val order: Int
+) {
+    EULER(label = "Усовершенствованный метод Эйлера", short = "Эйлер", order = 2),
+    RUNGE(label = "Метод Рунге-Кутта 4-го порядка", short = "Рунге-Кутта", order = 4),
+    MILNE(label = "Метод Милна", short = "Милна", order = 4),
 }
